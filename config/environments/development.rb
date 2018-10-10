0d0ee71d98d76085c024832901ea108774b53cdd
Daniel Sibaja <siba2893@gmail.com>
2018-10-09 20:40:32 -0600

    Update Email Sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,12 +40,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['email_username'],
-      :password => ENV['email_password'],
-      :address => 'smtp.mailtrap.io',
-      :domain => 'smtp.mailtrap.io',
-      :port => '2525',
-      :authentication => :cram_md5
+      :user_name      => ENV['email_username'],
+      :password       => ENV['email_password'],
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
   }
 
   # Print deprecation notices to the Rails logger.
